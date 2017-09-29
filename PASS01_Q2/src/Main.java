bb21c3a98f9f3a620c2fbb2b94ba93745aee8cdb
Lenkplenk <leon.kalish@Outlook.com>
2017-09-29 13:36:39 -0400

    zzz

---
+++ PASS01_Q2/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int userX;
+		int userY;
+		double rndX;
+		double rndY;
+		int score = 10000;
+		double distance= Math.sqrt((rndY-userY)^2+(rndX-userX)^2);
+		double scoreLoss;
+		int rangeHigh;
+		int rangeLow;
+		
+		scoreLoss = distance / 10;
+
+		rndX = Math.random();
+
+	}
+
+}
